e391900f498a30636ef10a22a9b9456b470688f9
hudhill <sam.hudhill@gmail.com>
2021-04-28 13:08:15 +0100

    finished

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from modules.output import *
-from data.task_list import tasks
 from modules.input import *
+from modules.task_list import print_menu
+tasks = []
 
-def print_menu():
-    print("Options:")
-    print("1: Display All Tasks")
-    print("2: Get Uncompleted Tasks")
-    print("3: Get Completed Tasks")
-    print("4: Mark Task as Complete")
-    print("5: Get Tasks Which Take Longer Than a Given Time")
-    print("6: Find Task by Description")
-    print("7: Add a new Task to list")
-    print("Q or q: Quit")
+import_old = input("Would you like to load previous tasks? (y/n)")
+if import_old == "y":
+    from data.task_list import tasks
 
 while (True):
     print_menu()
